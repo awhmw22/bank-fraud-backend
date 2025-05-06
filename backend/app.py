b01de6f4fc2a482fe0cef19e51f4d47e8f1e8792
awhmw22 <wattoohuzaifa18@gmail.com>
2025-05-06 17:17:45 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -96,4 +96,8 @@ def reset_password():
 
 # ----------------- MAIN -----------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
